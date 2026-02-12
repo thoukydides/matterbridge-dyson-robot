5cb55e57daaac0c4bb1230bdc7dee4b9662d1bd3
Alexander Thoukydides <thoukydides@users.noreply.github.com>
2026-02-12 08:47:17 +0000

    Matterbridge 3.5.4 compatibility

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 // Matterbridge plugin for Dyson robot vacuum and air treatment devices
 // Copyright © 2025-2026 Alexander Thoukydides
 
-import { Matterbridge, PlatformConfig } from 'matterbridge';
+import { PlatformMatterbridge, PlatformConfig } from 'matterbridge';
 import { AnsiLogger } from 'matterbridge/logger';
 import { PlatformDyson } from './platform.js';
 
 // Register the platform with Matterbridge
 export default function initializePlugin(
-    matterbridge:   Matterbridge,
+    matterbridge:   PlatformMatterbridge,
     log:            AnsiLogger,
     config:         PlatformConfig
 ): PlatformDyson {
